8785d90838bbe0a4f6e832c09f5c5178d74d4bb4
Yenuli Minsadi <samararatney@gmail.com>
2025-10-08 11:24:46 +0530

    implement server side socket

---
+++ Socket-Programming/src/main/java/edu/clientserver/Client.java
@@ -0,0 +1,12 @@
+package edu.clientserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Socket-Programming/src/main/java/edu/clientserver/Server.java
@@ -0,0 +1,26 @@
+package edu.clientserver;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(3000);
+            System.out.println("server started");
+
+            Socket localSocket = serverSocket.accept();//accepted request has been assigned to local socket after creating local socket
+            System.out.println("client accepted");
+
+            DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());//get and read received data stream
+            String message = dataInputStream.readUTF();//convert data to readable string
+            System.out.println("Client: "+message);
+            
+            localSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
