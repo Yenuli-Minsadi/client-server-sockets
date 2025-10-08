6c0b6b9f557ade55c703130481bd160a22e8cf20
Yenuli Minsadi <samararatney@gmail.com>
2025-10-08 11:43:46 +0530

    implement input client-server

--- Socket-Programming/src/main/java/edu/clientserver/Client.java
+++ Socket-Programming/src/main/java/edu/clientserver/Client.java
@@ -4,15 +4,22 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Client {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         try {
             Socket remoteSocket = new Socket("localhost", 3000);
             DataOutputStream dataOutputStream = new DataOutputStream(remoteSocket.getOutputStream());
-            dataOutputStream.writeUTF("hello server");
+            System.out.print("Input message: ");
+            String input = scanner.next();
+            dataOutputStream.writeUTF(input);
+
+            dataOutputStream.flush();
+            remoteSocket.close();
         }catch (IOException e) {
             throw new RuntimeException(e);
         }
