de692a0b9c5d88d9351d1796dfe7d58342b92aa3
Yenuli Minsadi <samararatney@gmail.com>
2025-10-09 10:26:27 +0530

    implement cli chat app

--- Socket-Programming/src/main/java/edu/clientserver/Client.java
+++ Socket-Programming/src/main/java/edu/clientserver/Client.java
@@ -12,21 +12,27 @@ import java.util.Scanner;
 public class Client {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        try {
-            Socket remoteSocket = new Socket("localhost", 3000);
-            DataOutputStream dataOutputStream = new DataOutputStream(remoteSocket.getOutputStream());
-            System.out.print("Input message: ");
-            String input = scanner.next();
-            dataOutputStream.writeUTF(input);
 
-            DataInputStream dataInputStream = new DataInputStream(remoteSocket.getInputStream());//get and read received data stream
-            String message = dataInputStream.readUTF();//convert data to readable string
-            System.out.println("Server: "+message);
+            try {
+                    Socket remoteSocket = new Socket("localhost", 3000);
+                boolean flag = true;
+                while (flag) {
+                    DataOutputStream dataOutputStream = new DataOutputStream(remoteSocket.getOutputStream());
+                    System.out.print("Input message: ");
+                    String input = scanner.next();
+                    dataOutputStream.writeUTF(input);
 
-            dataOutputStream.flush();
-            remoteSocket.close();
-        }catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+                    DataInputStream dataInputStream = new DataInputStream(remoteSocket.getInputStream());//get and read received data stream
+                    String message = dataInputStream.readUTF();//convert data to readable string
+                    System.out.println("Server: " + message);
+
+                    dataOutputStream.flush();
+                }
+
+                remoteSocket.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
     }
 }
--- Socket-Programming/src/main/java/edu/clientserver/Server.java
+++ Socket-Programming/src/main/java/edu/clientserver/Server.java
@@ -10,27 +10,32 @@ import java.util.Scanner;
 public class Server {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(3000);
-            System.out.println("server started");
 
-            Socket localSocket = serverSocket.accept();//accepted request has been assigned to local socket after creating local socket
-            System.out.println("client accepted");
+            try {
+                ServerSocket serverSocket = new ServerSocket(3000);
+                System.out.println("server started");
 
-            DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());//get and read received data stream
-            String message = dataInputStream.readUTF();//convert data to readable string
-            System.out.println("Client: "+message);
+                Socket localSocket = serverSocket.accept();//accepted request has been assigned to local socket after creating local socket
+                System.out.println("client accepted");
 
-            DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
-            System.out.print("Input message: ");
-            String input = scanner.next();
-            dataOutputStream.writeUTF(input);
-            dataOutputStream.flush();
+                boolean flag = true;
+                while (flag) {
+                    DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());//get and read received data stream
+                    String message = dataInputStream.readUTF();//convert data to readable string
+                    System.out.println("Client: " + message);
+
+                    DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
+                    System.out.print("Input message: ");
+                    String input = scanner.next();
+                    dataOutputStream.writeUTF(input);
+
+                    dataOutputStream.flush();
+                }
+                    localSocket.close();
 
 
-            localSocket.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
     }
 }
